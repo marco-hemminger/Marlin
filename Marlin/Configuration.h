56ecdcb49f1724c5290d7a31f7a67df09b41bacd
Marco Hemminger <marco.hemminger@gmail.com>
2020-05-17 15:23:22 +0200

    Fix extruder step per milimeter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2133.33, 210 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2133.33, 419 }
 
 /**
  * Default Max Feed Rate (mm/s)
